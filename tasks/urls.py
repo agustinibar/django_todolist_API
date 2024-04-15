a86409d196867cd31e50a1841894dacfce25c1bf
agustinibar <agustinibarperrotta@gmail.com>
2024-04-15 14:46:27 -0300

    reinstall

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework.documentation import include_docs_urls
 from  tasks import views
 
 router = routers.DefaultRouter()
@@ -7,5 +8,6 @@ router = routers.DefaultRouter()
 router.register(r'tasks', views.TaskView, 'tasks')
 
 urlpatterns = [
-    path("api/v1/", include(router.urls))
+    path("api/v1/", include(router.urls)),
+    path("docs/", include_docs_urls()),
 ]
