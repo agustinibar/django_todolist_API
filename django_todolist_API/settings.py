6370c21e37cfb2d9e4371936b223c218eb4e1d1a
agustinibar <agustinibarperrotta@gmail.com>
2024-04-15 15:02:17 -0300

    finish backend

--- django_todolist_API/settings.py
+++ django_todolist_API/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'rest_framework',
+    'rest_framework',   
     'coreapi',
     'tasks',
 ]
@@ -129,3 +129,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #cors autohrization
 CORS_ALLOWED_ORIGINS = []
+
+REST_FRAMEWORK = {
+    ...: ...,
+    "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
+}
